f2701988c4140c5684c0a714bb6097b8d1c1b9a3
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2022-06-17 17:05:40 -0400

    added tequilala job

--- shared/jobs.lua
+++ shared/jobs.lua
@@ -555,6 +555,17 @@ QBShared.Jobs = {
 			['4'] = { name = 'Manager', isboss = true, payment = 150 },
         },
 	},
+    ['tequilala'] = {
+		label = 'Tequilala',
+		defaultDuty = false,
+		grades = {
+            ['0'] = { name = 'Recruit', payment = 50 },
+			['1'] = { name = 'Novice', payment = 75 },
+			['2'] = { name = 'Experienced', payment = 100 },
+			['3'] = { name = 'Advanced', payment = 125 },
+			['4'] = { name = 'Manager', isboss = true, payment = 150 },
+        },
+	},
     ['mailcarrier'] = {
 		label = 'Go Postal',
 		defaultDuty = true,
