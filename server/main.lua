1fd1332cfec1fadb6c783603be4acb560d8accd5
donutsforains <113471914+donutsforains@users.noreply.github.com>
2022-12-03 23:49:00 -0500

    updated

--- server/main.lua
+++ server/main.lua
@@ -8,6 +8,77 @@ exports('GetCoreObject', function()
     return QBCore
 end)
 
+RegisterNetEvent('qb-phone:server:sendNewMail', function(mailData)
+    local src = source
+    local phoneNumber = exports["lb-phone"]:GetEquippedPhoneNumber(src)
+    if phoneNumber then
+        local email = exports["lb-phone"]:GetEmailAddress(phoneNumber)
+        if email then
+            if mailData.button and mailData.button.enabled then
+                exports["lb-phone"]:SendMail({
+                    to = email,
+                    sender = mailData.sender,
+                    subject = mailData.subject,
+                    message = mailData.message,
+                    actions = {
+                        {
+                            label = mailData.button.buttonData.locationLabel,
+                            data = {
+                                event = mailData.button.buttonEvent,
+                                isServer = false,
+                                data = mailData.button.buttonData
+                            }
+                        },
+                    }
+                })
+            else
+                exports["lb-phone"]:SendMail({
+                    to = email,
+                    sender = mailData.sender,
+                    subject = mailData.subject,
+                    message = mailData.message,
+                })
+            end
+        end
+    end
+end)
+
+RegisterNetEvent('qb-phone:server:sendNewMailToOffline', function(citizenid, mailData)
+    local phoneNumber = exports["lb-phone"]:GetEquippedPhoneNumber(citizenid)
+    if phoneNumber then
+        local email = exports["lb-phone"]:GetEmailAddress(phoneNumber)
+        if email then
+            if mailData.button and mailData.button.enabled then
+                exports["lb-phone"]:SendMail({
+                    to = email,
+                    sender = mailData.sender,
+                    subject = mailData.subject,
+                    message = mailData.message,
+                    actions = {
+                        {
+                            label = mailData.button.buttonData.locationLabel,
+                            data = {
+                                event = mailData.button.buttonEvent,
+                                isServer = false,
+                                data = mailData.button.buttonData
+                            }
+                        },
+                    }
+                })
+            else
+                exports["lb-phone"]:SendMail({
+                    to = email,
+                    sender = mailData.sender,
+                    subject = mailData.subject,
+                    message = mailData.message,
+                })
+            end
+        end
+    else
+        print("[lb-phone-Compatability] Phone Number not found for:", citizenid)
+    end
+end)
+
 -- To use this export in a script instead of manifest method
 -- Just put this line of code below at the very top of the script
 -- local QBCore = exports['qb-core']:GetCoreObject()
